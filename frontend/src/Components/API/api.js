1abbb75f8c03b33ff999153c1c2a23cca4012ae4
HarishTPrasad <harish1090prasad@gmail.com>
2025-03-31 13:31:34 +0530

    fix

--- frontend/src/Components/API/api.js
+++ frontend/src/Components/API/api.js
@@ -7,18 +7,18 @@ import axios from "axios";
 // const api = axios.create({
 //     baseURL: "http://dc2-backend-1:5000/api", 
 //   });
-// const api = axios.create({
-//   baseURL: "http://10.0.1.221/api/form",
-// });
+const api = axios.create({
+  baseURL: "http://10.0.1.221/api/form",
+});
 // const api = axios.create({
 //   baseURL: "/api" // Let Nginx handle routing
 // });
 
-const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? '/api' 
-    : 'http://dc2-backend-1:5000/api'
-});
+// const api = axios.create({
+//   baseURL: process.env.NODE_ENV === 'production' 
+//     ? '/api' 
+//     : 'http://dc2-backend-1:5000/api'
+// });
   
 
 export default api;
