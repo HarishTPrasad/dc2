eef506d175877a517f11cb1bb6ed307f642d4ad9
HarishTPrasad <harish1090prasad@gmail.com>
2025-03-31 13:53:50 +0530

    fix

--- frontend/src/Components/API/api.js
+++ frontend/src/Components/API/api.js
@@ -1,24 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-// API instance
-// const api = axios.create({
-//   baseURL: "http://localhost:5000/api", // Update this if needed
-// });
-// const api = axios.create({
-//     baseURL: "http://dc2-backend-1:5000/api", 
-//   });
+// For development (outside Docker)
+const devBaseURL = 'http://10.0.1.221:5000/api';
+
+// For production (inside Docker)
+const prodBaseURL = '/api';
+
 const api = axios.create({
-  baseURL: "http://10.0.1.221/api/form",
+  baseURL: process.env.NODE_ENV === 'production' ? prodBaseURL : devBaseURL
 });
-// const api = axios.create({
-//   baseURL: "/api" // Let Nginx handle routing
-// });
-
-// const api = axios.create({
-//   baseURL: process.env.NODE_ENV === 'production' 
-//     ? '/api' 
-//     : 'http://dc2-backend-1:5000/api'
-// });
-  
 
 export default api;
