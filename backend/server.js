16f1c2ce7830360e72c99265632a8e8d029aed39
HarishTPrasad <harish1090prasad@gmail.com>
2025-04-02 14:57:02 +0530

    harry

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
