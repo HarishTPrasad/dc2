2a36cc938d93d3aef16f9875b3ac7a297c429782
HarishTPrasad <harish1090prasad@gmail.com>
2025-04-10 12:49:31 +0530

    new update

--- frontend/src/Components/API/api.js
+++ frontend/src/Components/API/api.js
@@ -1,18 +1,48 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://10.0.1.221:5000/api', 
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+
+// api.interceptors.request.use(config => {
+//   if (config.data instanceof FormData) {
+//     config.headers['Content-Type'] = 'multipart/form-data';
+//   }
+//   return config;
+// });
+
+// export default api;
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.1.221:5000/api', 
+  baseURL: 'http://10.0.1.221:5000/api',
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  withCredentials: false
 });
 
+api.interceptors.request.use(
+  (config) => {
+    if (config.data instanceof FormData) {
+      config.headers['Content-Type'] = 'multipart/form-data';
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-api.interceptors.request.use(config => {
-  if (config.data instanceof FormData) {
-    config.headers['Content-Type'] = 'multipart/form-data';
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('🔴 API Error:', error?.response || error.message);
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default api;
